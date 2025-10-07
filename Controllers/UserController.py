d5a4bd71541594601b920d4911f5f316b4e7fd21
rybin-yu <rybin.y89@mail.ru>
2025-10-07 16:18:42 +0500

    Смотри UserController

--- Controllers/UserController.py
+++ Controllers/UserController.py
@@ -2,42 +2,141 @@ from Models.Users import *
 from bcrypt import hashpw, gensalt, checkpw
 
 class UsersController:
-    #Метод вывода всех записей таблицы Роли
+    ''' Контроллер управления пользователем в системе
+    Предоставляет методы CRUD
+    Attributes:
+        Модель: Использует модель Users для работы с базой данных
+    '''
+
     @classmethod
     def get(cls):
+        '''Вывод всех пользователей из базы данных
+        Returns:
+            ModelSelect: список объектов всех пользователей из таблицы
+
+        Example:
+            users = UsersController.get()
+            for user in users:
+                print(user.login)
+        '''
         return Users.select()
 
     @classmethod
     def show(cls, id):
+        '''Находит пользователя по ID.
+
+         Args:
+            id (int): Идентификатор пользователя
+
+        Returns:
+            Users or None: Объект пользователя или None если не найден
+
+        Example:
+            user = UsersController.show(1)
+            print(user.login if user else "Not found")
+        '''
         return Users.get_or_none(id)
 
     @classmethod
     def show_login(cls,login):
+        '''Находит пользователя по логину.
+        Args:
+            login (str): Логин пользователя
+
+        Returns:
+            Users or None: Объект пользователя или None если не найден
+
+        Example:
+            user = UsersController.show_login("admin")
+            if user:
+                print(f"User found: {user.login}")
+        '''
         return Users.get_or_none(Users.login==login)
 
     @classmethod
     def add(cls, login, password, role_id):
+        """Создает нового пользователя.
+
+                Args:
+                    login (str): Логин пользователя
+                    password (str): Пароль в открытом виде
+                    role_id (int): ID роли пользователя
+
+                Note:
+                    Пароль сохраняется в открытом виде. Рекомендуется использовать метод registration.
+
+                Example:
+                    UsersController.add("newuser", "password123", 2)
+                """
         Users.create(login=login,
                      password=password,
                      role_id=role_id)
 
     @classmethod
     def update(cls, id, **filds):
+        """Обновляет поля пользователя.
+
+                Args:
+                    id (int): ID пользователя для обновления
+                    **fields: Поля для обновления в виде словаря (ключ=значение)
+
+                Example:
+                    UsersController.update(1, login="newlogin", role_id=3)
+                """
         for key, value in filds.items():
             Users.update({key: value}).where(Users.id == id).execute()
 
     @classmethod
     def delete(cls,id):
+        """Удаляет пользователя по ID.
+
+                Args:
+                    id (int): ID пользователя для удаления
+
+                Example:
+                    UsersController.delete(1)
+                """
         Users.delete().where(Users.id == id).execute()
 
     @classmethod
     def registration(cls,login,password,role_id=1):
+        """Регистрирует нового пользователя с хешированием пароля.
+
+               Args:
+                   login (str): Логин пользователя
+                   password (str): Пароль в открытом виде
+                   role_id (int, optional): ID роли пользователя. По умолчанию 1 (обычный пользователь)
+
+               Security:
+                   Использует bcrypt для хеширования пароля
+
+               Example:
+                   UsersController.registration("newuser", "securepassword")
+               """
         hash_password = hashpw(password.encode('utf-8'),gensalt())
         Users.create(login=login,password=hash_password,role_id=role_id)
 
     #метод проверки логина и пароля - аунтификация
     @classmethod
     def auth(cls,login,password):
+        """Аутентифицирует пользователя по логину и паролю.
+
+               Args:
+                   login (str): Логин пользователя
+                   password (str): Пароль в открытом виде
+
+               Returns:
+                   bool: True если аутентификация успешна, иначе False
+
+               Security:
+                   Использует bcrypt для проверки хешированного пароля
+
+               Example:
+                  if UsersController.auth("user", "password"):
+                    print("Login successful")
+                  else:
+                    print("Invalid credentials")
+               """
         #проверить логин
         if Users.get_or_none(Users.login == login) != None:
             hspassword = Users.get_or_none(Users.login == login).password
